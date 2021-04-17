67093a2a558d8542ef1f45124258a2a40dddb7dc
thedoctormarc <36292102+thedoctormarc@users.noreply.github.com>
2021-04-17 12:31:37 +0200

    Forgot to commit. Ragdoll project

---
+++ Ragdoll Project/Assets/Scripts/CharacterLogic.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLogic : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 15f;
+    [SerializeField]  
+    ConfigurableJoint hipJoint;
+    [SerializeField]  
+    Rigidbody hipRigidBody;
+    [SerializeField]  
+    Animator animator;
+
+    bool walk = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(x, 0f, y).normalized;
+
+        if (dir.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(dir.z, dir.x) * Mathf.Rad2Deg;
+            hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+            hipRigidBody.AddForce(dir * speed);
+            walk = true;
+        }
+        else
+        {
+            walk = false;
+        }
+
+        animator.SetBool("walk", walk);
+    }
+}
---
+++ Ragdoll Project/Assets/Scripts/CopyJointRotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyJointRotation : MonoBehaviour
+{
+    [SerializeField]  
+    Transform targetLimb;
+    ConfigurableJoint joint;
+    Quaternion intiialRot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        intiialRot = targetLimb.transform.localRotation;
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        joint.targetRotation = copyRotation();
+    }
+
+    private Quaternion copyRotation()
+    {
+        return Quaternion.Inverse(targetLimb.localRotation) * intiialRot;
+    }
+}
