883238416654ef8befcd768ae3b64c86fd2e2bd6
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2021-05-08 20:22:25 +0200

    hdrp upgrade

---
+++ Ragdoll Project/Assets/TtorialShaders/MeteorVFX/Scripts/ProjectileMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    public GameObject impactprefab;
+    private Rigidbody rb;
+    public List<GameObject> trails;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(speed != 0 && rb != null)
+        {
+            rb.position += transform.forward * (speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+        if(impactprefab != null)
+        {
+            GameObject impactVFX = Instantiate(impactprefab, pos, rot) as GameObject;
+        }
+
+
+        if(trails.Count >0)
+        {
+            for(int i = 0; i < trails.Count; ++i)
+            {
+                trails[i].transform.parent = null;
+                ParticleSystem ps = trails[i].GetComponent<ParticleSystem>();
+                if(ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+            }
+        }
+        Destroy(gameObject);
+
+    }
+}
---
+++ Ragdoll Project/Assets/TtorialShaders/MeteorVFX/Scripts/spawnMeteor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnMeteor : MonoBehaviour
+{
+
+    public GameObject vfx;
+    public Transform startP;
+    public Transform endP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 startPos = startP.position;
+        GameObject objVFX = Instantiate(vfx, startPos,Quaternion.identity) as GameObject;
+        Vector3 endPos = endP.position;
+        RotateTo(objVFX, endPos);
+    }
+
+    // Update is called once per frame
+    void RotateTo(GameObject obj, Vector3 destination)
+    {
+        Vector3 direction = destination - obj.transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
