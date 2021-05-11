87d482b8575c9bc9fb2c2354ec676dc21b9f2b4e
GerardClotet <36155703+GerardClotet@users.noreply.github.com>
2021-05-11 13:51:20 +0200

    fun car animation

--- Ragdoll Project/Assets/Scripts/Car_Script.cs
+++ Ragdoll Project/Assets/Scripts/Car_Script.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class Car_Script : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    float speed;
     void Start()
     {
-        
+        speed = Random.Range(3.0f, 8.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.worldToLocalMatrix.MultiplyVector(new Vector3(0,0,-1f))*0.1f;
+        transform.position += transform.worldToLocalMatrix.MultiplyVector(new Vector3(0,0,-1f))*speed*Time.deltaTime;
     }
 }
